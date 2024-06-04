8f06d00de2ae67dc4fc7e217c2fdc705cf1937fb
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-06-04 20:52:39 +0900

    refactoring

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,22 @@
 def fizz():
     print('fizz')
 
-def buzz(): #정희
-    # 5의 배수에서 fizz 를 출력하는 함수를 작성해주세요.
-    pass
+def buzz(): 
+    print("buzz")
 
-def fizzbuzz(): #주송
-    # 15의 배수에서 fizz 를 출력하는 함수를 작성해주세요.
-    pass
+def fizzbuzz():
+    print("fizzbuzz")
 
 def play():
     for i in range(1,16):
-        if i % 3 == 0 and i % 5 != 0:
-            fizz()
-        elif :
-            buzz()
-        elif :
+        if i % 3 == 0 and i % 5 == 0:
             fizzbuzz()
+        elif i % 3 == 0:
+            fizz()
+        elif i % 5 == 0:
+            buzz()
+        else:
+            print(i)
     return
 
 play()
