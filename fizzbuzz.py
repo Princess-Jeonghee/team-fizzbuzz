c6dcdd4b841626ccdf04ace16daaea6c53f5f87e
khyeonm <122lamb@naver.com>
2024-06-04 20:22:31 +0900

    feat: Add play

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,23 @@
-print("ihihi")
+def fizz(): #민혁
+    # 3의 배수에서 fizz 를 출력하는 함수를 작성해주세요.
+    pass
+
+def buzz(): #정희
+    # 5의 배수에서 fizz 를 출력하는 함수를 작성해주세요.
+    pass
+
+def fizzbuzz(): #주송
+    # 15의 배수에서 fizz 를 출력하는 함수를 작성해주세요.
+    pass
+
+def play():
+    for i in range(1,16):
+        if :
+            fizz()
+        elif :
+            buzz()
+        elif :
+            fizzbuzz()
+    return
+
+play()
